d9fd193b555ea71d8f25f2690be54ff815ee89a1
harryArrouidaa <harryarrouida@gmail.com>
2024-04-30 21:38:54 +0100

    redesign the sidebar

--- social-app/resources/views/layouts/sidebar.blade.php
+++ social-app/resources/views/layouts/sidebar.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app')
 
-<div class="h-screen w-1/5 border-r-2 fixed top-0 left-0">
-    <div class="w-full flex flex-col mt-32 pl-10 gap-10">
-        <div class="flex items-center justify-start gap-5">
-            <img src="{{ asset('/assets/sidebar/profile.svg') }}" alt="not found" class="w-[30px]">
-            <a class="text-2xl text-slate-600 font-light  hover:underline" href="{{route('user.profile.view')}}">Profile</a>
+<div class="h-screen w-1/6 border-r-2 fixed top-0 left-0 bg-slate-50">
+    <div class="w-full flex flex-col mt-32 pl-10 gap-5 pt-32">
+        <div class="flex items-center justify-start gap-2">
+            <img src="{{ asset('/assets/sidebar/profile.svg') }}" alt="not found" class="w-[25px]">
+            <a class="text-xl text-slate-600 font-normal hover:underline" href="{{route('user.profile.view')}}">Profile</a>
         </div>
-        <div class="flex items-center justify-start gap-5">
-            <img src="{{ asset('/assets/sidebar/browse.svg') }}" alt="not found" class="w-[30px]">
-            <a class="text-2xl text-slate-600 font-light hover:underline" href="{{ route('posts.view') }}">Browse</a>
+        <div class="flex items-center justify-start gap-2">
+            <img src="{{ asset('/assets/sidebar/browse.svg') }}" alt="not found" class="w-[25px]">
+            <a class="text-xl text-slate-600 font-normal hover:underline" href="{{ route('posts.view') }}">Browse</a>
         </div>
-        <div class="flex items-center justify-start gap-5">
-            <img src="{{ asset('/assets/sidebar/new.svg') }}" alt="not found" class="w-[30px]">
-            <a class="text-2xl text-slate-600 font-light hover:underline" href="{{route('posts.new.view')}}">New</a>
+        <div class="flex items-center justify-start gap-2">
+            <img src="{{ asset('/assets/sidebar/new.svg') }}" alt="not found" class="w-[25px]">
+            <a class="text-xl text-slate-600 font-normal hover:underline" href="{{route('posts.new.view')}}">New</a>
         </div>
-        <div class="flex items-center justify-start">
-            <form action="{{ route('logout.action') }}" method="post" class="flex items-center  gap-5">
-                @csrf
-                <img src="{{ asset('/assets/sidebar/logout.svg') }}" alt="not found" class="w-[30px]">
-                <button type="submit" class="text-2xl text-slate-600 hover:underline font-light">Logout</button>
-            </form>
-        </div>
+    </div>
+    <div class="flex items-center justify-start absolute bottom-5 left-10">
+        <form action="{{ route('logout.action') }}" method="post" class="flex items-center gap-2">
+            @csrf
+            <img src="{{ asset('/assets/sidebar/logout.svg') }}" alt="not found" class="w-[25px]">
+            <button type="submit" class="text-xl text-slate-600 hover:underline font-normal">Logout</button>
+        </form>
     </div>
 </div>
--- social-app/resources/views/user/profile.blade.php
+++ social-app/resources/views/user/profile.blade.php
@@ -6,41 +6,6 @@
         @include('layouts.sidebar')
     </div>
     <div class="w-4/5 h-screen">
-
-        <div class="p-16">
-            <div class="p-8 bg-white mt-24">
-                <div class="grid grid-cols-1 md:grid-cols-3">
-                    <div class="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
-                        <div>
-                            <p class="font-bold text-gray-700 text-xl">22</p>
-                            <p class="text-gray-400">Followers</p>
-                        </div>
-                    </div>
-                    <div class="relative">
-                        {{-- profile --}}
-                        <div
-                            class="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
-                            <img src="{{ Storage::url(auth()->user()->profile->path) }}" alt="Profile Image"
-                                class="rounded-full w-[200px] h-[200px]">
-                        </div>
-                    </div>
-                    <div class="space-x-8 flex justify-end mt-32 md:mt-0 md:justify-center"><button
-                            class="btn btn-ghost btn-outline uppercase">
-                            Connect</button>
-                    </div>
-                </div>
-                <div class="mt-20 text-center pb-12">
-                    <h1 class="text-4xl font-medium text-gray-700">{{ auth()->user()->username }} <span
-                            class="font-light text-gray-500">{{ auth()->user()->age }}</span></h1>
-                    <p class="font-light text-gray-600 mt-3">{{ auth()->user()->address }}</p>
-                    {{-- <p class="mt-8 text-gray-500">work</p>
-                    <p class="mt-2 text-gray-500">study</p> --}}
-                </div>
-                <div class="mt-12 flex flex-col justify-center">
-                    <p class="text-gray-600 text-center font-light lg:px-16">An artist of considerable range, Ryan — the
-                        {{ auth()->user()->status }}</p>
-                </div>
-            </div>
-        </div>
+        
     </div>
 </div>
